122bed4458e924b7b568a5010a707bf93e210681
Pradeep Kumar <pradeep@Pradeeps-MacBook-Air.local>
2026-07-27 13:07:33 +0530

    Day 2: Arithmetic operators and decision making

---
+++ Phase-0/Day-02/decision_statement.py
@@ -0,0 +1,30 @@
+
+# Decison Making (if statement)
+
+name = input("Enter your Name: ")
+age = int(input("Enter your Age: "))
+
+if age >= 18:
+    print("Hello " + name + "!" + " You are an adult.")
+
+
+
+# Practice
+
+# Big Number Check
+number = int(input("Please enter a number: "))
+
+if number > 100:
+    print("Big Number")
+
+# Ask Password
+password = input("Please enter your password: ")
+
+if password == "python":
+    print("Access Granted")
+
+# Height Check for Fun Ride
+height = int(input("Please enter your height: ")
+)
+if height >= 150:
+    print("Ride Allowed")
