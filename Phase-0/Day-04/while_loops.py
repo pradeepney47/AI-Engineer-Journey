3e0fb571134d0ca5815ea06ef38301dd296a20c9
Pradeep Kumar <pradeep@Pradeeps-MacBook-Air.local>
2026-09-07 17:26:27 +0530

    Refreshed while loop concept again

--- Phase-0/Day-04/while_loops.py
+++ Phase-0/Day-04/while_loops.py
@@ -29,20 +29,48 @@ while password_typed != correct_password: # condition
 print("Access Granted.")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Break free from Infinite Samsaric Pattern
+def samsara(movement):
+    #if there is a self movement
+    if movement: 
+        #to defend one's self (via affect, cognition, and behaviour as one consciousness)
+        ego = 1
+        #self is empty (because there is no permanent self)
+        self = []
+        #while defending oneself is always trapped within consciousness pattern
+        while ego >= 0:
+            #consciousness being a source
+            if movement[ego] == "yang": 
+                #self is always experienced as presence
+                self += ["presence"]
+            #consciousness being a shadow
+            elif movement[ego] == "yin": 
+                #self is always experienced as absence
+                self += ["absence"]
+            #but by down regulating the defensive ego we can break away from this infinite loop
+            ego -= 1
+        #and can return to see the actual movement of self    
+        return self
+    else:
+        #self is empty but not it's movement
+        raise ValueError("Movement of self can never be empty!") 
+#duality in movement (only constant is movement of source and shadow)
+movement = ["yin", "yang"]
+# movement = []
+try:
+    #when we try to see the movement being passed inside samsaric function, 
+    #actual nirvana is attained
+    nirvana = samsara(movement)
+except ValueError as move_self_never_empty:
+    print()
+    #movement of self can never be empty (although self is empty)
+    print(move_self_never_empty) 
+else:
+    #nirvana is to break away from samsaric patterns by understanding the movement of self is non-dual
+    #and it is the co-existence of presence and absence
+    print()
+    print("Self is a movement of " + " and ".join(nirvana), "!")
+    print()
 
 
 
